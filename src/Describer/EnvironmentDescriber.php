924fca881a7d82de3b7cb7cf70df503b40d0f86f
Markus Poerschke <markus@poerschke.nrw>
2021-04-13 21:48:10 +0200

    Fix Psalm errors

--- src/Describer/EnvironmentDescriber.php
+++ src/Describer/EnvironmentDescriber.php
@@ -29,11 +29,13 @@ class EnvironmentDescriber
     public function describe(Environment $twig): Documentation
     {
         $functionDescriptions = new DescriptionCollection();
+        /** @psalm-suppress InternalMethod This package must access the functions to output a description. */
         foreach ($twig->getFunctions() as $function) {
             $functionDescriptions = $functionDescriptions->with($this->functionDescriber->describe($function));
         }
 
         $filterDescriptions = new DescriptionCollection();
+        /** @psalm-suppress InternalMethod This package must access the filters to output a description. */
         foreach ($twig->getFilters() as $filter) {
             $filterDescriptions = $filterDescriptions->with($this->filterDescriber->describe($filter));
         }
